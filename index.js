54c56e9bca2029ae43526ccfe9cfe57e418fdde3
Jason Garber <jason@sixtwothree.org>
2023-02-07 15:35:28 -0500

    Try writing message data to a separate file

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+const fs = require('node:fs');
 const process = require('node:process');
 
-console.log('Parsing inbound client payload...');
+const message = JSON.parse(process.argv[2]);
 
-process.argv.slice(2).forEach(value => {
-  return console.log(JSON.parse(value));
-});
+if (message.type && message.type.toLowerCase() === 'follow') {
+  const follower = message.actor;
+  
+  console.log(`Processing follow request from ${follower}...`);
+  
+  const followers = JSON.parse(fs.readFileSync('./followers.json'));
+  
+  if (followers.includes(follower)) {
+    console.log(`${follower} is already a follower. Skipping...`);
+  } else {
+    console.log('Updating ./followers.json...');
+    
+    fs.writeFileSync('./followers.json', JSON.stringify(followers.push(follower)));
+  }
+} else {
+  console.log(`Message is of type ${message.type}. Skipping...`);
+}
