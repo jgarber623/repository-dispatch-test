39f77c7afc287d1a7c93bea50cb1d6a00128a917
Jason Garber <jason@sixtwothree.org>
2023-02-07 15:11:36 -0500

    Log process.argv

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const process = require('node:process');
 
 console.log('Parsing inbound client payload...');
 
+console.log(process.argv);
+
 process.argv.each(value => {
   return console.log(JSON.parse(value));
 });
